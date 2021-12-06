7f318243133663d963dee058cb8add5e83418961
Husni and nijin <93671844+nijin-husni@users.noreply.github.com>
2021-12-06 21:29:13 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,45 +1,39 @@
-const Maalutty = require('../events');
+const Dude_v2 = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Maalutty.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Dude_v2.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
     var r_text = new Array ();
     
     
-    r_text[0] = "https://i.ibb.co/JHqKPX5/Screenshot-2021-11-26-22-39-45-1.png";
+    r_text[0] = "https://i.ibb.co/nRqrzc2/20211205-102026.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `❤️Maalutty_v2💙
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `❤️Dude_v2💙
 
 *bot making video* : https://youtu.be/jF8wF1KdyOw
 
 
-*owner number husni* :https://wa.me/917025868709.
+*owner number Devil* :https://wa.me/918848477621.
             
 
-*owner number Nijin* :https://wa.me/919605385305.
+*github* : https://github.com/Killady/Dude_v2.
 
 
-*owner id instagram* :https://instagram.com/_husni_ser_.
+*audio commands* :https://github.com/Killady/uploads/tree/main/bgm.
 
-
-*github* : https://github.com/nijin-husni/Maalutty.
-
-
-*audio commands* :https://github.com/nijin-husni/uploads/tree/main/bgm.
-
-*sticker commands* :https://github.com/nijin-husni/uploads/tree/main/stickers.
+*sticker commands* :https://github.com/Killady/uploads/tree/main/stickers.
 ■□■□■□■□■□■□■□■□■□■□
-❤️Maalutty💙
-▣▣ ~NIJIN~ AND ~HUSNI~ ▣▣
+❤️Dude_v2💙
+▣▣ ~Devil~ AND ~Maalutty~ ▣▣
 
 `}) 
 
